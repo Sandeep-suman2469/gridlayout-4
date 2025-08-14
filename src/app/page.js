74b7603286e29ab69cd7039c8f2b2e9fa5622346
Sandy3395 <suman.sandeep1999@gmail.com>
2025-08-14 18:17:43 +0530

    grid-4

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,70 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+    <div className="nav">
+        <div className="layout navbar">Layout4</div>
+        <div className="about navbar">about</div>
+        <div className="services navbar">services</div>
+        <div className="contact navbar">contact</div>
+      </div>
+      <div className="container">
+          <div className="box-1">
+            <h1 className="heading">Blog Post Title</h1>
+            <div className="sec-text">
+               <div>by<span className="test">Test</span></div>
+            </div>
+            <div className="date">⏰ Posted on August 24, 2013 at 9:00 PM</div>
+             <div className="box">900 x 300</div>
+             <div className="para">
+               <div className="p">Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus
+                 ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis
+                  tempus leo eu aenean sed diam.</div>
+               <div className="p1">Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus.</div>
+               <div className="p1">Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus.</div>
+               <div className="p1">Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus.</div>
+               <div className="p1">Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus.</div>
+               <div className="copyright">Copyright © Your Website 2014</div>
+             </div>
+          </div>
+          <div className="box-2">
+             <div className="searchbar">
+                <h2 className="h2">Blog Search</h2>
+                <div className="search">
+                <input type="text"></input>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                            <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+                            </svg> */}
+                      </div>
+             </div>
+             <div className="aside">
+               <h2 className="h2">Blog Catogeries</h2>
+               <div className="para3">
+                  <div className="p3">
+                     <div className="p4">Category Name</div>
+                     <div className="p4">Category Name</div>
+                     <div className="p4">Category Name</div>
+                     <div className="p4">Category Name</div>
+                  </div>
+                  <div className="p3">
+                     <div className="p4">Category Name</div>
+                     <div className="p4">Category Name</div>
+                     <div className="p4">Category Name</div>
+                     <div className="p4">Category Name</div>
+                  </div>
+               </div>
+             </div>
+             <div className="footer">
+               <div className="para1">Side Widge Well</div>
+               <div className="para2">
+                   Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae
+                   pellentesque sem placerat in id cursus mi.
+               </div>
+             </div>
+          </div>
+      </div>
+      
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
